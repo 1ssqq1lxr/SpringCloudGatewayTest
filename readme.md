7e131476deb0b770caf6981607351fb9f196bf10
luxurong <luxurong520@hellobike.com>
2021-04-14 11:38:42 +0800

    init

---
+++ readme.md
@@ -0,0 +1,8 @@
+
+
+## 环境 jdk8+  本机装有docker
+
+
+测试限流代码在 : GatewayRateLimiterTest
+测试限流代码在 : GatewayRetryTest
+测试限流代码在 : GatewayRetryTest
---
+++ src/main/java/com/1ssqq1lxr/services/gateway/model/Account.java
@@ -0,0 +1,15 @@
+package pl.piomin.services.gateway.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Account {
+
+	private Integer id;
+	private String number;
+
+}
